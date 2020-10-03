4d7699a7f93a9359d429bbf93dd953ffc6a091f8
MaxSymonov <maxsimonov9@gmail.com>
2020-10-04 00:22:46 +0300

    Added data providers

--- src/test/java/com/telran/tests/alltests/BoardCreationNewTests.java
+++ src/test/java/com/telran/tests/alltests/BoardCreationNewTests.java
@@ -5,6 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -21,6 +22,20 @@ public class BoardCreationNewTests extends TestBase {
         return list.iterator();
     }
 
+    @DataProvider
+    public Iterator<Object[]> validBoardsFromCSV() throws IOException {
+        List<Object[]> list = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new FileReader(new File("src/test/resources/boards.csv")));
+        String line = br.readLine();
+        while (line != null){
+            String[] split = line.split(",");
+            list.add(new Object[]{new Board().setBoardName(split[0]).setTeamVisible(split[1])});
+            line = br.readLine();
+        }
+
+        return list.iterator();
+    }
+
     @Test(dataProvider = "validBoards")
     public void boardCreation(String boardName, String teamVisibility) {
         int before = app.getBoard().getBoardsCount();
@@ -28,6 +43,19 @@ public class BoardCreationNewTests extends TestBase {
         app.getHeader().selectCreateBoard();
         Board board = new Board().setBoardName(boardName).setTeamVisible(teamVisibility);
         //app.getBoard().fillBoardForm(new Board().setBoardName("NewTeam").setTeamVisible("public"));
+        app.getBoard().fillBoardForm(board);
+        app.getBoard().confirmBoardCreation();
+        app.getHeader().returnToHomepageFromBoard();
+        int after = app.getBoard().getBoardsCount();
+        System.out.println("was: " + before + " now: " + after);
+        Assert.assertEquals(after, before + 1);
+    }
+
+    @Test(dataProvider = "validBoardsFromCSV")
+    public void boardCreationFromCSV(Board board) {
+        int before = app.getBoard().getBoardsCount();
+        app.getHeader().clickOnPlusButton();
+        app.getHeader().selectCreateBoard();
         app.getBoard().fillBoardForm(board);
         app.getBoard().confirmBoardCreation();
         app.getHeader().returnToHomepageFromBoard();
