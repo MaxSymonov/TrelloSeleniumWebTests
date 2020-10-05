42354913b67e20fff4ec7f6a8cd08416b780f383
MaxSymonov <maxsimonov9@gmail.com>
2020-10-05 22:56:36 +0300

    Added team data provider

--- src/test/java/com/telran/tests/alltests/TeamTests.java
+++ src/test/java/com/telran/tests/alltests/TeamTests.java
@@ -2,16 +2,36 @@ package com.telran.tests.alltests;
 
 import com.telran.tests.model.Team;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class TeamTests extends TestBase {
 
-    @Test
-    public void teamTests() {
+    @DataProvider
+    public Iterator<Object[]> teams() throws IOException {
+        List<Object[]> list = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new FileReader(new File("/src/test/resources/teams.csv")));
+        String line = reader.readLine();
+        while (line != null){
+            String[] split = line.split(",");
+            list.add(new Object[]{new Team().setTeamName(split[0]).setNewDescription(split[1]).setNewTag(split[2])});
+            line = reader.readLine();
+        }
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "teams")
+    public void teamTests(String teamName, String newDescription, String newTag) {
         int before = app.getTeam().getTeamCount();
-        app.getTeam().createTeam(new Team().setTeamName("TestBoard" + System.currentTimeMillis()));
+        //app.getTeam().createTeam(new Team().setTeamName("TestBoard" + System.currentTimeMillis()));
+        app.getTeam().createTeam(new Team().setTeamName(teamName));
         app.getTeam().removeTeam();
-        app.getTeam().teamModify(new Team().setTeamName("NewName" + System.currentTimeMillis()).setNewDescription("New description").setNewTag("tag" + System.currentTimeMillis()));
+        Team team = new Team().setTeamName(teamName).setNewDescription(newDescription).setNewTag(newTag);
         app.getHeader().returnOnHomePageFromTeam();
         int after = app.getTeam().getTeamCount();
         Assert.assertNotEquals(after, before);
